8bd44f471879b7cf6741501a4f378694165a3242
joacoseo1 <joacoseo1@gmail.com>
2025-11-17 22:05:40 -0300

    ajustes en terraform ecr

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,6 @@ output "load_balancer_url" {
   value       = aws_lb.main.dns_name
 }
 
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.app.repository_url
-}
-
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
   value       = aws_ecs_cluster.main.name
