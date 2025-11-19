51b9431a02d3611f06914e03954a7d99a3d6e117
joacoseo1 <joacoseo1@gmail.com>
2025-11-19 18:19:43 -0300

    Cambios en main.tf y variables.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -156,8 +156,8 @@ resource "aws_lb" "main" {
 }
 
 # Target Group
-resource "aws_lb_target_group" "app" {
-  name        = "ecs-target-group"
+resource "aws_lb_target_group" "api_gateway" {
+  name        = "tg-${var.ecs_service_name_api_gateway}"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
@@ -176,7 +176,55 @@ resource "aws_lb_target_group" "app" {
   }
 
   tags = {
-    Name = "ecs-target-group"
+    Name = "tg-${var.ecs_service_name_api_gateway}"
+  }
+}
+
+resource "aws_lb_target_group" "product_service" {
+  name        = "tg-${var.ecs_service_name_product_service}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "tg-${var.ecs_service_name_product_service}"
+  }
+}
+
+resource "aws_lb_target_group" "inventory_service" {
+  name        = "tg-${var.ecs_service_name_inventory_service}"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "tg-${var.ecs_service_name_inventory_service}"
   }
 }
 
@@ -193,8 +241,38 @@ resource "aws_lb_listener" "front_end" {
 }
 
 # ECR Repository
-resource "aws_ecr_repository" "app" {
-  name                 = var.ecr_repository_name
+resource "aws_ecr_repository" "api_gateway" {
+  name                 = var.ecr_repository_name_api_gateway
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  force_delete = true
+
+  tags = {
+    Name = var.ecr_repository_name
+  }
+}
+
+resource "aws_ecr_repository" "product_service" {
+  name                 = var.ecr_repository_name_product_service
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  force_delete = true
+
+  tags = {
+    Name = var.ecr_repository_name
+  }
+}
+
+resource "aws_ecr_repository" "inventory_service" {
+  name                 = var.ecr_repository_name_inventory_service
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
@@ -228,8 +306,8 @@ resource "aws_cloudwatch_log_group" "ecs" {
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "app" {
-  family                   = var.ecs_service_name
+resource "aws_ecs_task_definition" "api_gateway" {
+  family                   = var.ecs_service_name_api_gateway
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -238,9 +316,9 @@ resource "aws_ecs_task_definition" "app" {
 
   container_definitions = jsonencode([
     {
-      name      = var.container_name
-      image     = "${aws_ecr_repository.app.repository_url}:latest"
-      essential = true
+      name        = "api-gateway"
+      image       = "${aws_ecr_repository.api_gateway.repository_url}:latest"
+      essential   = true
       portMappings = [
         {
           containerPort = 80
@@ -260,16 +338,88 @@ resource "aws_ecs_task_definition" "app" {
   ])
 
   tags = {
-    Name = var.ecs_service_name
+    Name = var.ecs_service_name_api_gateway
+  }
+}
+
+resource "aws_ecs_task_definition" "product_service" {
+  family                   = var.ecs_service_name_product_service
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = data.aws_iam_role.lab_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "product-service"
+      image       = "${aws_ecr_repository.product_service.repository_url}:latest"
+      essential   = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Name = var.ecs_service_name_product_service
+  }
+}
+
+resource "aws_ecs_task_definition" "inventory_service" {
+  family                   = var.ecs_service_name_inventory_service
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = data.aws_iam_role.lab_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name        = "inventory-service"
+      image       = "${aws_ecr_repository.inventory_service.repository_url}:latest"
+      essential   = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+
+  tags = {
+    Name = var.ecs_service_name_inventory_service
   }
 }
 
 # ECS Service
-resource "aws_ecs_service" "main" {
-  name            = var.ecs_service_name
+resource "aws_ecs_service" "api_gateway" {
+  name            = var.ecs_service_name_api_gateway
   cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 2
+  task_definition = aws_ecs_task_definition.api_gateway.arn
+  desired_count   = var.ecs_desired_count
   launch_type     = "FARGATE"
 
   network_configuration {
@@ -279,15 +429,67 @@ resource "aws_ecs_service" "main" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.app.arn
-    container_name   = var.container_name
+    target_group_arn = aws_lb_target_group.api_gateway.arn
+    container_name   = "api-gateway"
     container_port   = 80
   }
 
   depends_on = [aws_lb_listener.front_end]
 
   tags = {
-    Name = var.ecs_service_name
+    Name = var.ecs_service_name_api_gateway
+  }
+}
+
+resource "aws_ecs_service" "product_service" {
+  name            = var.ecs_service_name_product_service
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.product_service.arn
+  desired_count   = var.ecs_desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.product_service.arn
+    container_name   = "product-service"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.front_end]
+
+  tags = {
+    Name = var.ecs_service_name_product_service
+  }
+}
+
+resource "aws_ecs_service" "inventory_service" {
+  name            = var.ecs_service_name_inventory_service
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.inventory_service.arn
+  desired_count   = var.ecs_desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.inventory_service.arn
+    container_name   = "inventory-service"
+    container_port   = 80
+  }
+
+  depends_on = [aws_lb_listener.front_end]
+
+  tags = {
+    Name = var.ecs_service_name_inventory_service
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,50 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
-variable "ecr_repository_name" {
-  description = "Name of the ECR repository"
-  type        = string
-  default     = "my-web-app"
-}
-
+# ECS Cluster
 variable "ecs_cluster_name" {
-  description = "Name of the ECS cluster"
+  description = "Name of the ECS Cluster"
   type        = string
-  default     = "my-cluster"
 }
 
-variable "ecs_service_name" {
-  description = "Name of the ECS service"
+# ECS Services Names
+variable "ecs_service_name_api_gateway" {
+  description = "ECS Service name for api-gateway"
   type        = string
-  default     = "my-service"
 }
 
-variable "container_name" {
-  description = "Name of the container"
+variable "ecs_service_name_product_service" {
+  description = "ECS Service name for product-service"
   type        = string
-  default     = "web-app"
+}
+
+variable "ecs_service_name_inventory_service" {
+  description = "ECS Service name for inventory-service"
+  type        = string
+}
+
+# ECR Repository Names
+variable "ecr_repository_name_api_gateway" {
+  description = "Name of API Gateway ECR repository"
+  type        = string
+}
+
+variable "ecr_repository_name_product_service" {
+  description = "Name of Product Service ECR repository"
+  type        = string
+}
+
+variable "ecr_repository_name_inventory_service" {
+  description = "Name of Inventory Service ECR repository"
+  type        = string
+}
+
+# Desired count for all ECS services
+variable "ecs_desired_count" {
+  description = "Number of tasks to run for each ECS service"
+  type        = number
+  default     = 1
 }
